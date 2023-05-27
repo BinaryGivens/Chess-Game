2b6fa50ff8e1718c87b3392bd44a16194b3e1edb
BinaryGivens <127585475+BinaryGivens@users.noreply.github.com>
2023-05-27 16:53:16 -0700

    Update pawnType.cpp

--- pawnType.cpp
+++ pawnType.cpp
@@ -1,3 +1,6 @@
+/*
+Description: This file has the pawn movement function
+*/
 #include "pawnType.h"
 using  namespace std;
 #include <iostream>
@@ -11,9 +14,10 @@ bool pawnType::move(int startRow, int startCol, int endRow,
 {
     int sC = startCol;
 	int eC = endCol;
-    
+    // this handles the movements for a black pawn first time moving
     if (board[startRow][sC]->moveCount == 0 && board[startRow][sC]->getPlayerType() == false)
     {  
+	    // checks if the pawn is capturing a piece for its first move
         if (endRow - startRow == 1 && abs(eC-sC) == 1)
             {
                 if (board[endRow][eC] != nullptr)
@@ -28,6 +32,7 @@ bool pawnType::move(int startRow, int startCol, int endRow,
                 else
                     return false;
             }
+	    // this checks if a pawn is moving up 2 spaces for its first turn
         if (endRow - startRow == 2 && board[endRow][eC] == nullptr && eC - sC == 0)
         {
             if (board[startRow+1][eC]== nullptr)
@@ -39,6 +44,7 @@ bool pawnType::move(int startRow, int startCol, int endRow,
                 return false;
             }
         }
+	    // this checks if a pawn is moving by only one for its first turn
         if (endRow - startRow == 1 && eC - sC == 0)
         {
             if (board[endRow][eC] == nullptr)
@@ -55,8 +61,10 @@ bool pawnType::move(int startRow, int startCol, int endRow,
             return false;
         }
     }
+	// this handles the red pawn movements for its first move
     if (board[startRow][sC]->moveCount == 0 && board[startRow][sC]->getPlayerType() == true)
     {
+	    // checks if the pawn is capturing a piece for its first move
         if (endRow - startRow == -1 && abs(eC-sC) == 1)
         {
             if (board[endRow][eC] != nullptr)
@@ -71,6 +79,7 @@ bool pawnType::move(int startRow, int startCol, int endRow,
             else
                 return false;
         }
+	     // this checks if a pawn is moving up 2 spaces for its first turn
         if (endRow - startRow == -2 && board[endRow][eC] == nullptr && eC - sC == 0)
         {
             if (board[startRow-1][eC]== nullptr)
@@ -82,6 +91,7 @@ bool pawnType::move(int startRow, int startCol, int endRow,
                 return false;
             }
         }
+	    // this checks if a pawn is moving by only one for its first turn
         if (endRow - startRow == -1 && eC - sC == 0)
         {
             if (board[endRow][eC] == nullptr)
@@ -99,10 +109,13 @@ bool pawnType::move(int startRow, int startCol, int endRow,
             return false;
         }
     }
+	// this handles the basic movements for a pawn including moving up by 1 and capturing after its first move
     else
     {
+	    // checks the movements for the red pawns
         if (board[startRow][sC]->getPlayerType() == true)
         {
+		// checks if the red pawn is capturing a piece
             if (endRow - startRow == -1 && abs(eC-sC) == 1)
             {
                 if (board[endRow][eC] != nullptr)
@@ -117,8 +130,10 @@ bool pawnType::move(int startRow, int startCol, int endRow,
                 else
                     return false;
             }
+		// checks the red pawns basic movement
             if (endRow-startRow == -1 && eC - sC == 0)
             { 
+		    // if the space is empty return true
                 if (board[endRow][eC] == nullptr)
                 {
                     return true;
@@ -129,8 +144,10 @@ bool pawnType::move(int startRow, int startCol, int endRow,
                 }
             }
         }
+	    //checks the movements for the black pawns
         if (board[startRow][sC]->getPlayerType() == false)
         {
+		// checks if black pawn is capturing a piece
             if (endRow - startRow == 1 && abs(eC-sC) == 1)
             {
                 if (board[endRow][eC] != nullptr)
@@ -145,8 +162,10 @@ bool pawnType::move(int startRow, int startCol, int endRow,
                 else
                     return false;
             }
+		// checks if the black pawn is moving forward
             if (endRow - startRow == 1 && eC - sC == 0)
             { 
+		    // if the spapce is empty return true
                 if (board[endRow][eC] == nullptr)
                 {
                     return true;
