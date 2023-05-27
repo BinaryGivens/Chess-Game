30a94cb3926781ed7a2cb4d2ed0ca1cd8ea89c80
BinaryGivens <127585475+BinaryGivens@users.noreply.github.com>
2023-05-27 16:42:31 -0700

    Update chessPiece.cpp

--- chessPiece.cpp
+++ chessPiece.cpp
@@ -1,13 +1,5 @@
 /*
-Name: Markiece Givens, NSHE: 2001489836, 1003, ASSIGNMENT 5
-Description: This assignment is a chess game that allows the user
-to make moves and check to see if the move is valid. It also
-alternates players turns between black and red.
-Input: The user inputs an initial move that must correlate to a
-piece on the board. The user then inputs a final move that must
-either contain a nullptr or a piece of the opposite color.
-Output: The output is the board with the pieces in their positions.
-It also displays black and red pieces.
+Description: inherited class that sets eats chesspieces color, player and handles prints when a piece is removed
 */
 #include <iostream>
 #include "chessPiece.h"
